7244c9afe68570c52f9ac9c507be7c2a8ee4f5c3
Kyle Stahl <stahl085@umn.edu>
2020-02-22 21:56:27 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'bracketology',         
   packages = ['bracketology'],   
-  version = '0.0.1',     
+  version = '0.0.1-alpha',     
   license='MIT',        
   description = 'Analyze and simulate NCAA march madness tournaments',
   author = 'Kyle Stahl',                   
   author_email = 'stahl085@umn.edu',      
   url = 'https://github.com/stahl085/bracketology',   
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/stahl085/bracketology/archive/0.0.1-alpha.tar.gz',    # I explain this later on
   keywords = ['brackets', 'NCAA', 'basketball', 'march', 'madness', 'tournament'],   # Keywords that define your package best
   install_requires=[],
   classifiers=[
