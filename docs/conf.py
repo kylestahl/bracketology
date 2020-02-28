0672730d9964c5e2b3b7c67558101e7d94dcbd38
stahl085 <stahl085@umn.edu>
2020-02-27 19:32:39 -0600

    add requirements for docs

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ version = release
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'numpydoc'] # Should convert numpy or google style docstrings
+extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.napoleon'] # Should convert numpy or google style docstrings
 
 # Point to the master.rst
 master_doc = 'index'
